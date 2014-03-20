502c76807992ace23f49f1408866870d94a89d7a
Woecifaun <woecifaun@gmail.com>
2014-03-20 22:34:05 +0100

    add accents set to bépo charset

--- bepo.charsets.js
+++ bepo.charsets.js
@@ -2,12 +2,38 @@ var charsets = {
     lowercase: {
         label: 'minuscules',
         activate: true,
-        set: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        set: [
+            'a','b','c','d','e',
+            'f','g','h','i','j',
+            'k','l','m','n','o',
+            'p','q','r','s','t',
+            'u','v','w','x','y',
+            'z'
+        ]
     },
     uppercase: {
         label: 'majuscules',
         activate: true,
-        set: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+        set: [
+            'A','B','C','D','E',
+            'F','G','H','I','J',
+            'K','L','M','N','O',
+            'P','Q','R','S','T',
+            'U','V','W','X','Y',
+            'Z'
+            ]
+    },
+    loweraccent: {
+        label: 'à ê ï œ ù',
+        activate: true,
+        set: [
+            'à','â',
+            'ç',
+            'ê','è','é','ë',
+            'ï','î',
+            'œ','ô','ö',
+            'ù','û'
+            ]
     },
     ponctuation: {
         label: 'ponctuation',
